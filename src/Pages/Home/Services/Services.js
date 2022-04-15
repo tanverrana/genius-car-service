8bd0b80d8015833fdecece9cc0d2de9afcb22679
tanverrana <tanversobur@gmail.com>
2022-04-15 23:48:05 +0600

    create some components and install firebase

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h2>
+                This is services.
+            </h2>
+        </div>
+    );
+};
+
+export default Services;
