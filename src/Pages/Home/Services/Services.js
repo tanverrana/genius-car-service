ebf98e6284a2fc201e302ee06be2a61aa262f22f
tanverrana <tanversobur@gmail.com>
2022-04-16 04:48:36 +0600

    style and responsive

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import repair1 from "../../../images/repair1.png"
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Service from '../Home/Service/Service';
+import "./Services.css";
+
 
 
 
 const Services = () => {
 
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch("service.json")
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
+
     return (
         <div>
-            <h2>
-                This is services.
-            </h2>
+            <h2 className="services-title">Our Services:{services.length}</h2>
+            <div className="services-container">
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    >
+
+                    </Service>)
+                }
+            </div>
         </div>
     );
 };
