667a8b2a422fac5fd7eeadbb6fa1a533c43134c1
tanverrana <tanversobur@gmail.com>
2022-05-07 08:57:52 +0600

    load user order

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
 import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
 import ManageServices from './Pages/ManageServices/ManageServices';
+import Order from './Pages/Order/Order';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
@@ -38,6 +39,11 @@ function App() {
         <Route path="/manage" element={
           <RequireAuth>
             <ManageServices></ManageServices>
+          </RequireAuth>
+        }></Route>
+        <Route path="/orders" element={
+          <RequireAuth>
+            <Order></Order>
           </RequireAuth>
         }></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
---
+++ src/Pages/Order/Order.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
+const Order = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOreders] = useState([]);
+    useEffect(() => {
+        const getOrders = async () => {
+            const email = user.email;
+            const url = `http://localhost:5000/order?email=${email}`;
+            const { data } = await axios.get(url);
+            setOreders(data);
+        }
+        getOrders();
+
+    }, [user])
+    return (
+        <div>
+            <h2>Your Orders:{orders.length}</h2>
+
+        </div>
+    );
+};
+
+export default Order;
