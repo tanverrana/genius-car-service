58f94040f92fb6120f33b5cfb556d158ff6cd709
tanverrana <tanversobur@gmail.com>
2022-05-07 10:18:03 +0600

    show tokne

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -10,6 +10,7 @@ import { toast } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
 import PageTitle from '../../Shared/PageTitle/PageTitle';
+import axios from 'axios';
 
 const Login = () => {
     const emailRef = useRef("");
@@ -33,7 +34,7 @@ const Login = () => {
         return <Loading></Loading>
     }
     if (user) {
-        navigate(from, { replace: true });
+        //navigate(from, { replace: true });
     }
     if (error) {
         errorElement = <div>
@@ -41,12 +42,15 @@ const Login = () => {
         </div>
     }
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
-        console.log(email, password);
-        signInWithEmailAndPassword(email, password);
+
+        await signInWithEmailAndPassword(email, password);
+        const { data } = await axios.post('http://localhost:5000/login', { email });
+        localStorage.setItem('accessToken', data.accessToken);
+        navigate(from, { replace: true });
     }
 
     const navigateRegister = event => {
