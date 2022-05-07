7efddf472df84a6acd7966f9192a3587953035e3
tanverrana <tanversobur@gmail.com>
2022-05-07 07:09:55 +0600

    required proceed chekout page

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useServiceDetail from '../../../hooks/useServiceDetail';
 import PageTitle from '../../Shared/PageTitle/PageTitle';
 
 const Checkout = () => {
+    const { serviceId } = useParams();
+    const [service] = useServiceDetail(serviceId);
+
     return (
         <div>
 
             <h2>Please Checkout your booking!!</h2>
+            <div className="w-50 mx-auto">
+                <h3>Order Now:{service.name}</h3>
+                <form>
+                    <input className="w-1oo mb-2" type="text" name="name" placeholder="name" required />
+                    <br />
+                    <input className="w-1oo mb-2" type="email" name="email" placeholder="email" required />
+                    <br />
+                    <input className="w-1oo mb-2" type="text" value={service.name} name="service" placeholder="service" required />
+                    <br />
+                    <input className="w-1oo mb-2" type="text" name="address" placeholder="address" required />
+                    <br />
+                    <input className="w-1oo mb-2" type="text" name="phone" placeholder="phone" required />
+                    <br />
+                    <input type="submit" className="btn btn-primary" value="Place Order" />
+                </form>
+            </div>
         </div>
     );
 };
